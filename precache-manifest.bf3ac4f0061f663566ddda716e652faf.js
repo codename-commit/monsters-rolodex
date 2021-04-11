b669c7728c97c0460decd89d375f66104e6271f3
Thabo James Maboya <james.maboya@gmail.com>
2021-04-11 21:57:28 +0200

    Updates

---
+++ precache-manifest.bf3ac4f0061f663566ddda716e652faf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a2409fa1ee486326499318b86859fc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1c04c33e718402022903",
+    "url": "/monsters-rolodex/static/css/main.18fc2d1a.chunk.css"
+  },
+  {
+    "revision": "9c4c94738bc80fc9fa4c",
+    "url": "/monsters-rolodex/static/js/2.5ab3663c.chunk.js"
+  },
+  {
+    "revision": "1c04c33e718402022903",
+    "url": "/monsters-rolodex/static/js/main.9af3fe8e.chunk.js"
+  },
+  {
+    "revision": "15e85ea4c7b6758d19df",
+    "url": "/monsters-rolodex/static/js/runtime-main.cc812e55.js"
+  }
+]);
